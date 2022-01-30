0070a9fb132ad61f2f32d6dd07e7fcfa3366cf6e
hanilr <hakkianilragiboglu@gmail.com>
2022-01-31 01:49:59 +0300

    UI functions created with ascii escape sequence

--- src/lib/ui.hpp
+++ src/lib/ui.hpp
@@ -2,6 +2,10 @@
 #ifndef _UI_HPP
 #define _UI_HPP
 
+void clrscr(void);
 
+void cursor_visibility(bool state);
+
+void gotoxy(int pos_x, int pos_y);
 
 #endif /* MADE BY @hanilr */
--- src/ui.cpp
+++ src/ui.cpp
@@ -1,5 +1,20 @@
 /* TERMINAL FILE FINDER (USER INTERFACE) */
 
+// STANDARD LIBRARY
+#include <iostream>
 
+// DIY LIBRARY
+#include "lib/ui.hpp"
+#include "lib/config.hpp"
+
+void clrscr(void) { std::cout << clear_screen; }
+
+void cursor_visibility(bool state)
+{
+    if(state == true) { std::cout << cursor_visible; }
+    else { std::cout << cursor_invisible; }
+}
+
+void gotoxy(int pos_x, int pos_y) { printf("\033[%d;%dH", pos_y, pos_x); }
 
 /* MADE BY @hanilr */
