8e599c38885dde0f238500489a56ebeb5e71da02
hanilr <hakkianilragiboglu@gmail.com>
2022-02-03 22:19:25 +0300

    'nline()' and 'draw_frame' functions added

--- src/lib/ui.hpp
+++ src/lib/ui.hpp
@@ -8,4 +8,8 @@ void cursor_visibility(bool state);
 
 void gotoxy(int pos_x, int pos_y);
 
+void nline(int times); // NEW LINE
+
+void draw_frame(int x_len, int y_len, std::string frame_color);
+
 #endif /* MADE BY @hanilr */
--- src/ui.cpp
+++ src/ui.cpp
@@ -17,4 +17,22 @@ void cursor_visibility(bool state)
 
 void gotoxy(int pos_x, int pos_y) { printf("\033[%d;%dH", pos_y, pos_x); }
 
+void nline(int times) { for(int i = 0; times > i; i+=1) { std::cout << std::endl; } }
+
+void draw_frame(int x_len, int y_len, std::string frame_color)
+{
+    clrscr();
+    for(int i = 0; x_len > i; i+=1) { std::cout << frame_color << " " << esc_reset; }
+    nline(1);
+    for(int i = 1; y_len > i; i+=1)
+    {
+        gotoxy(0, i);
+        std::cout << frame_color << " " << esc_reset;
+        gotoxy(x_len, i);
+        std::cout << frame_color << " " << esc_reset;
+    }
+    nline(1);
+    for(int i = 0; x_len > i; i+=1) { std::cout << frame_color << " " << esc_reset; }
+}
+
 /* MADE BY @hanilr */
