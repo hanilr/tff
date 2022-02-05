5023bb391091ddfc300f4234d0e59c82379ac8d7
hanilr <hakkianilragiboglu@gmail.com>
2022-02-06 01:25:59 +0300

    'is_installed' function added. 'change_term_size()', 'add_color()', 'remove_color()' and 'save_in_data()' functions planned

--- src/conf.cpp
+++ src/conf.cpp
@@ -5,7 +5,25 @@
 
 // DIY LIBRARY
 #include "lib/conf.hpp"
+#include "lib/file.hpp"
 
+bool is_installed(void)
+{
+    set_path_to_main();
+    #ifdef _WIN32
+        std::string app_path = path_current() + "\\tff.exe";
+    #else
+        std::string app_path = path_current() + "/tff";
+    #endif
+    return is_file(app_path);
+}
 
+void change_term_size(int terminal_width, int terminal_height);
+
+void add_color(std::string color_name, std::string hex_color);
+
+void remove_color(std::string color_name);
+
+void save_in_data(std::string file_name);
 
 /* MADE BY @hanilr */
--- src/lib/conf.hpp
+++ src/lib/conf.hpp
@@ -2,6 +2,14 @@
 #ifndef _CONF_HPP
 #define _CONF_HPP
 
+bool is_installed(void);
 
+void change_term_size(int terminal_width, int terminal_height);
+
+void add_color(std::string color_name, std::string hex_color);
+
+void remove_color(std::string color_name);
+
+void save_in_data(std::string file_name);
 
 #endif /* MADE BY @hanilr */
