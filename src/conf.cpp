39c2e2f3b327cf7d3709b276b02841cd065ff0f3
hanilr <hakkianilragiboglu@gmail.com>
2022-02-05 21:50:21 +0300

    Initial commit

---
+++ src/conf.cpp
@@ -0,0 +1,11 @@
+/* TERMINAL FILE FINDER (CONFIGURATION) */
+
+// STANDARD LIBRARY
+#include <iostream>
+
+// DIY LIBRARY
+#include "lib/conf.hpp"
+
+
+
+/* MADE BY @hanilr */
---
+++ src/lib/conf.hpp
@@ -0,0 +1,7 @@
+/* TERMINAL FILE FINDER (CONFIGURATION) [HEADER] */
+#ifndef _CONF_HPP
+#define _CONF_HPP
+
+
+
+#endif /* MADE BY @hanilr */
