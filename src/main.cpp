0cbf701dbb76ff08ae7067f7c2b3254d78eec6ac
hanilr <hakkianilragiboglu@gmail.com>
2022-02-05 13:00:08 +0300

    Install and uninstall stabilized for unix

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char* argv[])
         if(install_decision == 'y')
         {
             // INSTALL SECTION
-            int term_size_x, term_size_y;
+            std::string term_size_x, term_size_y;
             clrscr();
             user_warn(term_x, term_y, 0, 0, colorfg_green, colorbg_gray, colorbg_white, "What is your terminal width?");
             gotoxy(divide_half(term_x)-3, divide_half(term_y)+2);
@@ -44,15 +44,51 @@ int main(int argc, char* argv[])
             std::cin >> term_size_y;
             
             #ifdef _WIN32
-                std::string app_path = "C:\\Program Files\\tff\\";
-                create_dir(app_path+"conf\\", true);
-                create_dir(app_path+"data\\", false);
-                system("g++ src/main.cpp src/file.cpp src/util.cpp src/ui.cpp -o C:\\Program Files\\tff\\tff.exe");
+                std::string main_dir = path_current();
+                std::string compile_com = "g++ " + main_dir + "/src/main.cpp " + main_dir + "/src/file.cpp " + main_dir + "/src/util.cpp " + main_dir + "/src/ui.cpp " + "-o ~/.tff/tff.exe";
+                std::string term_config = term_size_x + "\n" + term_size_y;
+                for(int x = 0, y = 10; y > x; x+=1)
+                {
+                    path_change("../");
+                    if(path_current().compare("C:\\") == 0)
+                    {
+                        path_change("Program Files\\");
+                        create_dir(".tff/conf/", true);
+                        create_dir(".tff/data/", false);
+                        create_file(".tff/conf/terminal.txt");
+                        write_file(".tff/conf/terminal.txt", term_config, 'w');
+                        system(compile_com.c_str());
+                        break;
+                    }
+                    if(x == 9)
+                    {
+                        clrscr();
+                        user_warn(term_x, term_y, 0, 0, colorfg_red, colorbg_gray, colorbg_red, "[ERROR] Root directory fault!");
+                    }
+                }
             #else
-                std::string app_path = "~/.tff/";
-                create_dir(app_path+"conf/", true);
-                create_dir(app_path+"data/", false);
-                system("g++ src/main.cpp src/file.cpp src/util.cpp src/ui.cpp -o ~/.tff/tff");
+                std::string main_dir = path_current();
+                std::string compile_com = "g++ " + main_dir + "/src/main.cpp " + main_dir + "/src/file.cpp " + main_dir + "/src/util.cpp " + main_dir + "/src/ui.cpp " + "-o ~/.tff/tff";
+                std::string term_config = term_size_x + "\n" + term_size_y;
+                for(int x = 0, y = 10; y > x; x+=1)
+                {
+                    path_change("../");
+                    if(path_current().compare("/") == 0)
+                    {
+                        path_change("home/" + get_username() + "/");
+                        create_dir(".tff/conf/", true);
+                        create_dir(".tff/data/", false);
+                        create_file(".tff/conf/terminal.txt");
+                        write_file(".tff/conf/terminal.txt", term_config, 'w');
+                        system(compile_com.c_str());
+                        break;
+                    }
+                    if(x == 9)
+                    {
+                        clrscr();
+                        user_warn(term_x, term_y, 0, 0, colorfg_red, colorbg_gray, colorbg_red, "[ERROR] Root directory fault!");
+                    }
+                }
             #endif
 
             clrscr();
@@ -64,9 +100,29 @@ int main(int argc, char* argv[])
     else if(argc > 1 && strcmp(argv[1], "-uninstall") == 0)
     {
         #ifdef _WIN32
-                std::string app_path = "C:\\Program Files\\tff\\tff.exe";
+                std::string app_path = "Program Files\\tff\\tff.exe";
+                for(int x = 0, y = 10; y > x; x+=1)
+                    {
+                        path_change("../");
+                        if(path_current().compare("C:\\") == 0) { break; }
+                        if(x == 9)
+                        {
+                            clrscr();
+                            user_warn(term_x, term_y, 0, 0, colorfg_red, colorbg_gray, colorbg_red, "[ERROR] Root directory fault!");
+                        }
+                    }
         #else
-                std::string app_path = "~/.tff/tff";
+                std::string app_path = "home/" + get_username() + "/.tff/tff";
+                for(int x = 0, y = 10; y > x; x+=1)
+                    {
+                        path_change("../");
+                        if(path_current().compare("/") == 0) { break; }
+                        if(x == 9)
+                        {
+                            clrscr();
+                            user_warn(term_x, term_y, 0, 0, colorfg_red, colorbg_gray, colorbg_red, "[ERROR] Root directory fault!");
+                        }
+                    }
         #endif
 
         if(is_file(app_path) == true)
@@ -82,19 +138,37 @@ int main(int argc, char* argv[])
             if(uninstall_decision == 'y')
             {
                 #ifdef _WIN32
-                    delete_file("C:\\Program Files\\tff\\tff.exe");
-                    delete_file("C:\\Program Files\\tff\\conf\\*.txt");
-                    delete_file("C:\\Program Files\\tff\\data\\*.txt");
-                    delete_dir("C:\\Program Files\\tff\\conf\\");
-                    delete_dir("C:\\Program Files\\tff\\data\\");
-                    delete_dir("C:\\Program Files\\tff\\");
+                    for(int x = 0, y = 10; y > x; x+=1)
+                    {
+                        path_change("../");
+                        if(path_current().compare("C:\\") == 0)
+                        {
+                            path_change("Program Files\\");
+                            delete_dir("tff/", true);
+                            break;
+                        }
+                        if(x == 9)
+                        {
+                            clrscr();
+                            user_warn(term_x, term_y, 0, 0, colorfg_red, colorbg_gray, colorbg_red, "[ERROR] Root directory fault!");
+                        }
+                    }
                 #else
-                    delete_file("~/.tff/tff");
-                    delete_file("~/.tff/conf/*.txt");
-                    delete_file("~/.tff/data/*.txt");
-                    delete_dir("~/.tff/conf/");
-                    delete_dir("~/.tff/data/");
-                    delete_dir("~/.tff");
+                    for(int x = 0, y = 10; y > x; x+=1)
+                    {
+                        path_change("../");
+                        if(path_current().compare("/") == 0)
+                        {
+                            path_change("home/" + get_username() + "/");
+                            delete_dir(".tff/", true);
+                            break;
+                        }
+                        if(x == 9)
+                        {
+                            clrscr();
+                            user_warn(term_x, term_y, 0, 0, colorfg_red, colorbg_gray, colorbg_red, "[ERROR] Root directory fault!");
+                        }
+                    }
                 #endif
                 clrscr();
                 user_warn(term_x, term_y, 0, 0, colorfg_green, colorbg_gray, colorbg_green, "Successfully uninstalled!");
